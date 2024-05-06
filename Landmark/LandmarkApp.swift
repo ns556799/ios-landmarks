262a9945c191823708bc07aa3032e64c794ae518
Niv Suresh <nivsuresh06@gmail.com>
2024-05-06 18:19:07 +0100

    Initial Commit

---
+++ Landmark/LandmarkApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandmarkApp.swift
+//  Landmark
+//
+//  Created by Niv Suresh on 06/05/2024.
+//
+
+import SwiftUI
+
+@main
+struct LandmarkApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
